3fcd2885003211deed591c791d4fb75dfda64c16
isuruAb <vpisabeywardana@gmail.com>
2018-09-20 15:00:48 +0800

    modify migrations and add mysql db

--- db/migrate/20180920064338_create_users.rb
+++ db/migrate/20180920064338_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :username null: false
-      t.string :password_digest null: false
-      t.boolean :active null: false, default: true
+      t.string :username, null: false, unique: true
+      t.string :password_digest, null: false
+      t.boolean :active, null: false, default: true
 
       t.timestamps
     end
