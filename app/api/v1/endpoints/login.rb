e1f40ed0e24b86b02a0570f6bbaec0aa98d68809
isuruAb <vpisabeywardana@gmail.com>
2018-10-05 00:04:02 +0800

    check password

--- app/api/v1/endpoints/login.rb
+++ app/api/v1/endpoints/login.rb
@@ -4,11 +4,9 @@ require 'bcrypt'
 module V1
   module ENDPOINTS
     class Login < Grape::API
-
       format :json
       prefix :api
       resource :users do
-
         desc "Creates and returns access_token if valid login"
         params do
           requires :username, type: String, desc: "Username or email address"
@@ -17,16 +15,11 @@ module V1
         post :login do
           user = User.find_by_username(params[:username].downcase)
           user_id=user.id
-          if true
-
-            payload={"user_id":user_id}
-
+          puts user
+          
+          if user.password==params[:password]
+            payload={"user_id":"user_id"}
             token = JWT.encode payload, ENV["SECRETKEY"], 'HS256'
-            
-            # eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoidGVzdCJ9.pNIWIL34Jo13LViZAJACzK6Yf0qnvT_BuwOxiMCPE-Y
-            puts token
-
-
             {token: token}
           else
             error!('Unauthorized.', 401)
