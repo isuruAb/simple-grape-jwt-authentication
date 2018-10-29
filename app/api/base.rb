ef947aa6b494b259a6033303e08643a6e8024ae6
isuruAb <vpisabeywardana@gmail.com>
2018-10-29 11:03:41 +0800

    Fixes:#2 Moved authentication into a helper

--- app/api/base.rb
+++ app/api/base.rb
@@ -1,5 +1,7 @@
 class Base < Grape::API
     
     version 'v1', using: :path
+    helpers AuthHelper
+
     mount V1::ENDPOINTS::Login
 end
--- app/api/v1/endpoints/login.rb
+++ app/api/v1/endpoints/login.rb
@@ -17,36 +17,23 @@ module V1
         post :login do
           @user = User.find_by_username(params[:username].downcase)
           user_id=@user.id.to_json
-          puts @user
-          #if @user.password == params[:password]
           if BCrypt::Password.new(@user.password_digest)==params[:password]
-            payload={"user_id":"user_id"}
-            token = JWT.encode payload, ENV["SECRETKEY"], 'HS256'
-            {token: token}
+          payload={"user_id":user_id}
+          token = JWT.encode payload, ENV["SECRETKEY"], 'HS256'
+          {token: token}
           else
-            error!('Unauthorized.', 401)
-
+          error!('Unauthorized.', 401)
           end
         end
       
         desc "Returns pong if logged in correctly"
         params do
-          #requires :token, type: String, desc: "Access token."
+            #requires :token, type: String, desc: "Access token."
         end
         get :ping do   
-          if "Bearer"==/Bearer/.match(headers['Authorization'])[0]
-            token=headers['Authorization'].split(" ")[1]
-            decoded_token = JWT.decode token, ENV["SECRETKEY"], true, { algorithm: 'HS256' }
-            { message: headers['Authorization'].split(" ")[1] }
-
-
-          else
-            error!('Unauthorized.', 401)
-
-          end
-
+            authenticate!
+            { authenticatedtoken: headers['Authorization'].split(" ")[1] }
         end
-
       end
     end
   end
